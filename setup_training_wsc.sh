8ecd1bd17c9196cda371835581ba5565eb109589
chandrab <chandrab@allenai.org>
2019-05-10 12:28:47 -0700

    Add option to script

--- setup_training_wsc.sh
+++ setup_training_wsc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python get_char_vocab.py
+python get_char_vocab.py --dataset wsc
 
 python filter_embeddings.py glove.840B.300d.txt data/dev.coref_format.jsonl data/train.coref_format.jsonl
 python cache_elmo.py data/dev.coref_format.jsonl data/train.coref_format.jsonl
